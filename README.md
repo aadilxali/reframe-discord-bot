2d7a2e7b3de373ea44c2b81960db86bb82e4abd9
Aadil Ali <aliaadil2002@gmail.com>
2022-04-13 02:30:06 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Reframe - a discord bot that frames your negative feelings in a more positive light
+
+## Add the bot to your discord server [here](https://discord.com/oauth2/authorize?client_id=847220707024175145&permissions=2048&scope=bot)! 
+#### Note: Type !reframe [your negative thought] in any text channel to use the bot :D
+
+Made this for 1729 x Repl.it's Discord bot challenge and inspired by my own experiences with mental health. It's easy to look at everything in a negative light and quickly spiral so it's helpful to have reminders that not everything that's happening to me is so bad. 
+
+It's also just really funny sometimes - here are some examples (feel free to PR/ tweet [@me](https://twitter.com/aadillpickle) with responses you get):
+
+![Example2](example2.png)
+
+![Example3](example3.png)
+
+![Example4](example4.png)
+
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import discord
+import openai
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+  if message.author != client.user:
+    if message.content.startswith('!reframe'):
+
+      prompt = ("The following is a list of negative thoughts which are then reframed with in a more positive, hopeful way:" + "\n\nNegative: this will never work out\nPositive: I'm having trouble right now, but it's not a big deal if this doesn't work out."
+      + "\nPositive: I'm having some trouble right now, but I'm making plans to solve those problems."
+      + "\n\nNegative: why do i suck at this\nPositive: I have learned a lot about this topic. I want to keep learning more about it so I'll keep up the effort.\nPositive: I know what I need to improve."
+      + "\n\nNegative: " + message.content.replace('!reframe', ''))
+
+      openai.api_key = os.environ['OPENAI_API_KEY']
+
+      response = openai.Completion.create(
+      engine="davinci",
+      prompt=prompt,
+      temperature=0.89,
+      max_tokens=60,
+      top_p=1.0,
+      frequency_penalty=0.0,
+      presence_penalty=0.0,
+      stop=["\n\n"]
+      )
+
+      messages = str(response["choices"][0]["text"]).split("Positive:")
+
+      msg = "Here's another way to think about that: " + messages[1] if len(messages) >= 2 else "Our robots seem to be overly negative right now. Please try again."
+      await message.channel.send(msg)
+
+token = os.environ['DISCORD_BOT_SECRET']
+client.run(token)
+
